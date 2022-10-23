fc905a3cf2f4f0a223bf75764ef790273da54611
wisang <wseom7@gmail.com>
2022-10-23 13:25:04 +0900

    mouse over and out effect implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import useState from "react";
 
 function App() {
+  const [heading, setHeading] = React.useState("Hello!");
+  const [isMouseOver, setMouseOver] = React.useState(false);
+  
+  function handleClick() {
+    setHeading("Submitted");
+  };
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{heading}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor: isMouseOver ? "black" : "white"}} onClick={handleClick}
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}>Submit</button>
     </div>
   );
 }
